7c3085c4219fa6073aef34124ea924f039555dfa
鬼刀 <zengfantao88888888@outlook.com>
2024-07-09 17:41:20 +0800

    setup配置项与option配置项的联系与区别

--- src/components/Person.vue
+++ src/components/Person.vue
@@ -5,6 +5,8 @@
     </div>
     <div>name: {{name}}</div>
     <div>age: {{age}}</div>
+    <button @click="setName">点击便名字</button>
+    <div>pusha: {{pusha}}</div>
   </div>
 </template>
 
@@ -14,7 +16,32 @@ export default {
   data() {
     return {
       name: '顶针',
-      age: 18
+      age: 18,
+      // data中是可以获取由setup产出的数据（未响应式）,但在ts代码层面会报错
+      pusha: this.sex
+    }
+  },
+  beforeCreate() {
+    console.log('beforeCreate')
+  },
+  // setup的执行顺序在所有生命周期钩子执行前
+  setup() {
+    console.log('setup')
+    // setup函数内部的数据优先级比data,methods高，会覆盖
+    let age = 18
+    let name = 'haru'
+    let sex: string = '奶啊你'
+
+    //暂时无法实现响应式数据
+    function setName() {
+      name = '梨花'
+    }
+
+    return {
+      age,
+      name,
+      sex,
+      setName
     }
   }
 }
