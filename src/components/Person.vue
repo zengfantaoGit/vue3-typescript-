1b4677ed74073f1f1be5d59ef2d500a2f63c9a99
鬼刀 <zengfantao88888888@outlook.com>
2024-07-09 18:01:08 +0800

    在script标签使用setup配置简化return操作

--- src/components/Person.vue
+++ src/components/Person.vue
@@ -6,45 +6,29 @@
     <div>name: {{name}}</div>
     <div>age: {{age}}</div>
     <button @click="setName">点击便名字</button>
-    <div>pusha: {{pusha}}</div>
   </div>
 </template>
 
-<script lang="ts">
-export default {
-  name: "Person",
-  data() {
-    return {
-      name: '顶针',
-      age: 18,
-      // data中是可以获取由setup产出的数据（未响应式）,但在ts代码层面会报错
-      pusha: this.sex
-    }
-  },
-  beforeCreate() {
-    console.log('beforeCreate')
-  },
-  // setup的执行顺序在所有生命周期钩子执行前
-  setup() {
-    console.log('setup')
-    // setup函数内部的数据优先级比data,methods高，会覆盖
-    let age = 18
-    let name = 'haru'
-    let sex: string = '奶啊你'
+<!--<script lang="ts">-->
+<!--export default {-->
+<!--  name: "Person",-->
+<!--}-->
+<!--</script>-->
 
-    //暂时无法实现响应式数据
-    function setName() {
-      name = '梨花'
-    }
+<!-- 在script中添加setup配置项，此时整个script标签包裹的就是setup函数内的函数体，且默认全部返回。但不能写入其他的配置项-->
+<!-- 若想要在script标签直接配置name配置项，则需要安装vite-plugin-vue-setup-extend开发依赖-->
+<script lang="ts" setup name="Person111">
+console.log('setup')
+// setup函数内部的数据优先级比data,methods高，会覆盖
+let age = 18
+let name = 'haru'
+let sex: string = '奶啊你'
 
-    return {
-      age,
-      name,
-      sex,
-      setName
-    }
-  }
+//暂时无法实现响应式数据
+function setName() {
+  name = '梨花'
 }
+
 </script>
 
 <style scoped>
--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,16 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+// 引入对应依赖
+import viteSetupExtend from 'vite-plugin-vue-setup-extend'
 // import { resolve } from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
+      //运行依赖
+    viteSetupExtend()
   ],
   resolve: {
     alias: {
