2cc43a219850a30f33ce382c511621aacdcb72be
鬼刀 <zengfantao88888888@outlook.com>
2024-07-10 23:56:34 +0800

    toRefs 与 toRef

--- src/components/Person.vue
+++ src/components/Person.vue
@@ -8,10 +8,8 @@
     <div>age: {{person.age}}</div>
     <button @click="setName">点击便名字</button>
     <button @click="setAge">添加一岁</button>
-    <button @click="setPerson">设置人</button>
     <hr>
-    <div>obj.a.b.c.d: {{obj.a.b.c.d}}</div>
-    <button @click="setObj">设置obj</button>
+    <div>person.obj.a: {{person.obj.a}}</div>
   </div>
 </template>
 
@@ -22,39 +20,46 @@
 
 
 <script lang="ts" setup name="Person">
-    import {reactive,ref} from 'vue'
-    // 若ref接收的是对象类型，内部其实也是调用了reactive函数。
-    // reactive无法接收基本数据类型作为参数
+    // 作用：将一个响应式对象中的每一个属性，转换为ref对象。且对象中的value与原响应式对象的相同属性引用相同
+    import {reactive,ref,toRefs} from 'vue'
+
     const person = reactive({
       name: 'haru',
-      age: 18
-    })
-    let obj = ref({
-      a:{
-        b:{
-          c:{
-            d:666
-          }
-        }
+      age: 18,
+      obj: {
+        a: 2
       }
     })
 
-    function setAge() {
-      person.age += 1
-    }
+    // let {name, age, obj} = person
+    // // haru 18 (直接使用解构赋值可以拿到响应式对象的值。
+    // console.log(name,age,obj)
+    //
+    // function setAge() {
+    //   age += 1
+    //   obj.a += 2
+    //   // 但这里的age只是拿到了person.age的值，无法一同修改
+    //   console.log(age,person.age)
+    //   // 解构赋值只是做到了浅拷贝。只拷贝了引用的值，所以对于对象obj内部的a值能做到一并修改
+    //   console.log(obj.a,person.obj.a)
+    // }
+
+    // toRefs会将一个响应式对象内部的所有键值对都会变成一对refImpl，且对应引用的地址相同
+    let {name,age,obj} = toRefs(person)
+    // ObjectRefImpl对象
+    console.log(name,age,obj)
     function setName() {
-      person.name = '梨花'
+      name.value = '梨花'
     }
 
-    function setPerson() {
-      // Cannot assign to 'person' because it is a constant.
-      // person = reactive({name: 'mo', age: 26}) (无法直接赋值）
-      Object.assign(person, {name: 'mo', age: 26}) // 只能通过Object.assign方法进行赋值才不会丢失响应式
+    function setAge() {
+      age.value += 1
+      obj.value.a += 2
+      // 此时age.value的值能与person.age的值做到同步
+      console.log(age.value,person.age)
+      console.log(obj.value.a,person.obj.a)
     }
 
-    function setObj() {
-      obj.value.a.b.c.d = 999
-    }
 
 </script>
 
