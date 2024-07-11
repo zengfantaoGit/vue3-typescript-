424d82bebd05e3f8bb17656b0170ccd76ff2bfd3
鬼刀 <zengfantao88888888@outlook.com>
2024-07-11 17:19:37 +0800

    监视【ref】定义的【基本类型】数据

--- src/components/Person.vue
+++ src/components/Person.vue
@@ -1,46 +1,31 @@
 <template>
   <div class="person">
-    <label for="first">首：</label>
-    <input type="text" v-model="firstName" id="fo=first"><br>
-    <label for="first">尾：</label>
-    <input type="text" v-model="LastName" id="fo=first"><br>
-    全名：{{fullName}}<br>
-    全名：{{fullName}}<br>
-    全名：{{fullName}}<br>
-    <hr>
-    <button @click="setNum">num</button>
+    <h1>情况一：监视【ref】定义的【基本类型】数据</h1>
+    <div>sum: {{sum}}</div>
+    <button @click="changeSum">点击sum++</button>
   </div>
 </template>
 
 
 <script lang="ts" setup name="Person">
-  // 引入computed计算属性函数
-  import {ref, computed} from "vue";
-  let firstName = ref('zhan')
-  let LastName = ref('san')
-  let num = 1
-
-  //修改了num但计算属性无变化.不会触发computed计算属性判断
-  function setNum() {
-    num += 1
-    console.log(num)
+  // 引入watch监听函数
+  import {watch,ref} from "vue";
+  let sum = ref(0)
+  // 修改方法
+  function changeSum() {
+    sum.value += 1
   }
 
-  // 若计算属性只携带一个函数，则该函数作为计算属性的getter，且fullName为只读属性
-  // 且计算属性具有缓存机制，在多次调用计算属性时会判断当前引用的（响应式数据）是否发生变化,若没有则会返回原先的缓存值，减少调用次数
-  // let fullName = computed(() => {
-  //   return firstName.value + '-' + LastName.value + num
-  // })
-
-  // 此时的fullName计算属性即可读取，也可以写入
-  let fullName = computed({
-    set(val) {
-      console.log(val)
-    },
-    get() {
-      return firstName.value + '-' + LastName.value + num
+  // 若监听ref基本数据。则直接填入不需要.value（此时监听value值的改变）
+  // 且watch函数会返回一个回调函数，调用后会结束此次监听任务
+  let stopWatch = watch(sum, (value, oldValue) => {
+    console.log(value,oldValue)
+    if (value >= 10) {
+      // 结束监听函数
+      stopWatch()
     }
   })
+  console.log(stopWatch)
 </script>
 
 <style scoped>
