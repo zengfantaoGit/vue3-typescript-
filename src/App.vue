8f9f6fcb9892318984491b0eaa8c26afdd2cf41a
鬼刀 <zengfantao88888888@outlook.com>
2024-07-12 00:02:00 +0800

    watchEffect与watch对比

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,21 @@
 <template>
   <div class="app">
-    <h1>
-      你好呀
-    </h1>
-    <person></person>
+    <h1 ref="hello">你好呀</h1>
+    <person ref="person"></person>
   </div>
 </template>
 
-<script lang="ts">
-import Person from "@/components/Person.vue";
-export default {
-  name: "App",
-  components: {Person}
-}
+<script lang="ts" setup name="App">
+  import Person from "@/components/Person.vue";
+  import {ref} from 'vue'
+  // 不加数据则返回以该名所设置的ref数据
+  // 在html标签添加则获取对应的dom元素
+  let hello = ref()
+  console.log('hello',hello)
+  // 在组件标签添加则获取组件实例对象，且默认无法获取子组件数据
+  let person = ref()
+  console.log('person',person)
+  let sum = ref()
 </script>
 
 <style scoped>
--- src/components/Person.vue
+++ src/components/Person.vue
@@ -1,50 +1,20 @@
 <template>
   <div class="person">
-    <h1>需求：水温达到50℃，或水位达到20cm，则联系服务器</h1>
-    <h2 id="demo">水温：{{temp}}</h2>
-    <h2>水位：{{height}}</h2>
-    <button @click="changePrice">水温+10</button>
-    <button @click="changeSum">水位+10</button>
-    <button @click="num += 1;count+= 1">num ++ </button>
+    <h1>北京</h1>
+    <h2 ref="hello">天安门</h2>
+    <h3>天津</h3>
   </div>
 </template>
 
 
 <script lang="ts" setup name="Person">
-  import {ref,watch,watchEffect} from "vue";
-  let temp = ref(0)
-  let height = ref(0)
-  let num = ref(0)
-  let count = 0
+  import {ref,defineExpose} from 'vue'
+  let a = ref(0)
+  let b = ref(1)
+  let c = ref(2)
 
-  function changePrice() {
-    temp.value += 10
-  }
-
-  function changeSum() {
-    height.value += 10
-  }
-
-  // 使用watch监视，则必须声明需要监视temp与height的ref对象
-  watch([temp,height], (value) => {
-    console.log('watch')
-    // 此时val: [temp,height]
-    let [newTemp, newHeight] = value
-    if (newTemp >= 50 || newHeight >= 20) {
-      console.log('发送服务器watch')
-    }
-  })
-
-  // 而使用watchEffect则不需要指明需要监督的属性。
-  // 且该watchEffect传入的函数会默认开始调用一次
-  const stopWatch = watchEffect(() => {
-    // count不是响应式数据，count的变化不会触发回调
-    console.log('watchEffect',count)
-    // 只有该回调函数用到的可响应式数据发生变化时才会重新触发回调
-    if (temp.value >= 50 || height.value >= 20) {
-      console.log('发送服务器watchEffect')
-    }
-  })
+  // defineExpose可以向引用它的父级暴露出自己所持有的数据
+  defineExpose({a,b,c})
 </script>
 
 <style scoped>
